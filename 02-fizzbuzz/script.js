7756d75900678a627da11dc4c9499df3d0138054
Aleksandr Petrov <petrov.aleksr@gmail.com>
2022-07-27 23:56:53 +0500

    Решена задача №2

--- 02-fizzbuzz/script.js
+++ 02-fizzbuzz/script.js
@@ -11,9 +11,28 @@
 */
 
 function fizzBuzz(num) {
-    // Напишите код здесь
-}
+    if (!num || !Number(num) || !Boolean(num) || num <= 0) {
+        console.log('Введены некорректные данные!');
+    }
+    else {
+        num = Number(num);
+        for (let i = 1; i <= num; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log('fizzbuzz');
+            }
+            else if (i % 3 === 0) {
+                console.log('fizz');
+            }
+            else if (i % 5 === 0) {
+                console.log('buzz');
+            }
+            else {
+                console.log(i);
+            }
+        }
+    }
+};
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(fizzBuzz(15));
+console.log(fizzBuzz(25));
